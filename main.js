8bac8e07558a00159c437babd4bff409b86e9532
rakibul-cse-diu <rakibul2180@gmail.com>
2022-03-03 22:37:48 +0600

    input pi added

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// generate pin
 const generatePinBtn = document.querySelector(".generate-btn");
+const displayPin = document.getElementById("generateDis");
 const numBtn = document.querySelectorAll(".button");
+const displayInput = document.getElementById("inputDis");
 
+// generate pin
 generatePinBtn.addEventListener("click", () => {
-    const displayPin = document.getElementById("generateDis");
     let randomNumber = Math.floor((Math.random() * (9999 - 1000 + 1)) + 9999);
     displayPin.value = randomNumber;
 })
@@ -13,6 +14,13 @@ generatePinBtn.addEventListener("click", () => {
 numBtn.forEach(btn => {
     btn.addEventListener("click", (e) => {
         const triggredBtn = e.target.innerText;
+
+        if (triggredBtn == "C") {
+            displayInput.value = '';
+        } else {
+            displayInput.value = displayInput.value + triggredBtn;
+        }
+
         console.log(triggredBtn);
     });
 })
